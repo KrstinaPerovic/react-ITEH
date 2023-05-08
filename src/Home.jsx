57d3e9f3a548f05c84946737b62c080a70e7937f
KrstinaPerovic <perovickrstina1@gmail.com>
2023-05-08 19:33:10 +0200

    dodata komponenta za pocetnu stranicu

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home (){
+ 
+    return (
+      <div className="home">
+        <img src="https://influencermatchmaker.co.uk/img/asset/YXNzZXRzL3RyYXZlbC12bG9nZ2Vycy1iYW5uZXItMTY1ODIyMjU1MC5qcGc=?fm=webp&s=18f61ffb49ad5af320559a758f067267" alt="Travel Image" />
+        <div className="home-content">
+          <h1>Welcome to our Travel Website</h1>
+          <p>Discover amazing destinations around the world and plan your next adventure today!</p>
+        
+        </div>
+      </div>
+    );
+   
+}
+
+export default Home;
