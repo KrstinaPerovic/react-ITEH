19bc312386d776398fa3a7017867526431a49c50
KrstinaPerovic <perovickrstina1@gmail.com>
2023-05-08 19:45:15 +0200

    kreirana reusable komponenta navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
  
 import './App.css';
 import Home from './Home';
+import Navbar from './Navbar';
 import Putovanja from './Putovanja';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ function App() {
   
   return (
       <BrowserRouter>
+      <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route> 
           <Route path="/putovanja" element={<Putovanja></Putovanja>}></Route> 
